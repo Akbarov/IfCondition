0d872b70e37459518cddc8d2cbc1d8dc2ddbba9f
Zohidjon Akbarov <zohidjonakbarov@gmail.com>
2021-08-27 11:03:28 +0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val birinchiSon = 10
+    val ikkinchiSon = 20
+
+    if (birinchiSon > ikkinchiSon) {
+        println("Birinchi son katta")
+    }else if (birinchiSon == ikkinchiSon) {
+        println("Ikkila son teng")
+    } else {
+        println("Ikkichi son katta")
+    }
+}
